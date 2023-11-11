867dbb9b52f543468801a31ac2dc64e5c0232f3a
theKnightsOfRohan <shadowsovereign100@gmail.com>
2023-11-10 19:55:33 -0800

    Add command line argument handling and list of commands

--- src/Program.cs
+++ src/Program.cs
@@ -1,12 +1,72 @@
 ﻿using System;
+using System.ComponentModel;
 
-namespace HelloWorld;
+namespace Main;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello, world!");
+        if (args.Length == 0)
+        {
+            WriteAllCommands();
+        }
+        else
+        {
+            HandleArgs(args);
+        }
     }
-}
 
+    public static void HandleArgs(string[] args)
+    {
+        foreach (string arg in args)
+        {
+            switch (ParseArg(arg))
+            {
+                case "v":
+                    Console.WriteLine("Version: 1.0.0");
+                    break;
+            }
+        }
+    }
+
+    public static string ParseArg(string arg)
+    {
+        if (arg.StartsWith("-"))
+        {
+            if (arg.StartsWith("--"))
+            {
+                arg = arg[2..];
+            }
+            else
+            {
+                arg = arg[1..];
+            }
+
+            if (arg.Length > 1)
+            {
+                arg = arg[..1];
+            }
+        }
+
+        return arg.ToLower();
+    }
+
+    public static void WriteAllCommands()
+    {
+        try
+        {
+            string[] commands = File.ReadAllLines("commands/commands.txt");
+            foreach (string command in commands)
+            {
+                Console.WriteLine(command);
+            }
+        }
+        catch (Exception e)
+        {
+            Console.WriteLine(e.Message);
+        }
+    }
+
+
+}
