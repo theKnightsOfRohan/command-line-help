51af8201eac3323f444aa33156b41a392e191cfd
theKnightsOfRohan <shadowsovereign100@gmail.com>
2023-11-10 21:21:10 -0800

    Add unit tests for ParseArg method

--- Program.Tests/UnitTest1.cs
+++ Program.Tests/UnitTest1.cs
@@ -1,3 +1,5 @@
+using Program;
+
 namespace Program.Tests;
 
 public class Tests
@@ -8,8 +10,38 @@ public class Tests
     }
 
     [Test]
-    public void Test1()
+    public void TestParseArgWithDoubleDash()
     {
-        Assert.Pass();
+        string arg = "--testArg";
+        string expected = "t";
+        string actual = Program.ParseArg(arg);
+        Assert.AreEqual(expected, actual);
+    }
+
+    [Test]
+    public void TestParseArgWithSingleDash()
+    {
+        string arg = "-testArg";
+        string expected = "t";
+        string actual = Program.ParseArg(arg);
+        Assert.AreEqual(expected, actual);
+    }
+
+    [Test]
+    public void TestParseArgWithoutDash()
+    {
+        string arg = "testArg";
+        string expected = "testarg";
+        string actual = Program.ParseArg(arg);
+        Assert.AreEqual(expected, actual);
+    }
+
+    [Test]
+    public void TestParseArgWithEmptyString()
+    {
+        string arg = "";
+        string expected = "";
+        string actual = Program.ParseArg(arg);
+        Assert.AreEqual(expected, actual);
     }
 }
